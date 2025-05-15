5aa8005817de5ae89ad28f1c900c60d88afdd41a
acikgozb <acikgozb@proton.me>
2025-05-15 03:06:57 +0300

    refactor(nmcli): Create Nmcli struct
    The basic subcommands (status, toggle) did not need a separate struct
    since the commands were quite simple and different from each other.
    
    However, the features and commands associated with `nmcli connection
    show` take many different forms and become a bit more convoluted based
    on available options. This was noticed during the initial development of
    `wl list-networks` feature.
    
    Therefore, a struct is created to have methods that provide:
    
    - Better intention (global options, subcommands, and subcommand opts),
    - Reducing unnecessary duplicated codes (stderr delegation),
    - A single way to essentially call `nmcli`.
    
    Signed-off-by: acikgozb <acikgozb@proton.me>

--- src/nmcli.rs
+++ src/nmcli.rs
@@ -18,39 +18,80 @@ impl fmt::Display for WiFiStatus {
     }
 }
 
-pub fn get_active_connections() -> Result<Vec<String>, Error> {
-    let mut nmcli = Command::new("nmcli");
-    let cmd = nmcli
-        .args(["-g", "NAME,DEVICE"])
-        .arg("connection")
-        .arg("show")
-        .arg("--active")
-        .output()?;
+struct Nmcli<'a> {
+    global_args: Vec<&'a str>,
+    subcommands: Vec<&'a str>,
+    subcommand_args: Vec<&'a str>,
+}
 
-    if !cmd.status.success() {
-        let nmcli_err = cmd.stderr.lines().collect::<Result<String, Error>>()?;
-        return Err(Error::other(nmcli_err));
+impl<'a> Nmcli<'a> {
+    fn new() -> Self {
+        Self {
+            global_args: vec![],
+            subcommands: vec![],
+            subcommand_args: vec![],
+        }
     }
 
-    let active_conn_device_pairs = cmd.stdout.lines().collect::<Result<Vec<String>, Error>>()?;
+    fn with_global_args(&mut self, args: Vec<&'a str>) -> &mut Self {
+        self.global_args = args;
+        self
+    }
+
+    fn with_subcommands(&mut self, cmds: Vec<&'a str>) -> &mut Self {
+        self.subcommands = cmds;
+        self
+    }
+
+    fn with_subcommand_args(&mut self, args: Vec<&'a str>) -> &mut Self {
+        self.subcommand_args = args;
+        self
+    }
+
+    fn exec(self) -> Result<Vec<u8>, Error> {
+        let mut nmcli = Command::new("nmcli");
+        let cmd = nmcli
+            .args(self.global_args)
+            .args(self.subcommands)
+            .args(self.subcommand_args)
+            .output()?;
+
+        if !cmd.status.success() {
+            let nmcli_err = cmd.stderr.lines().collect::<Result<String, Error>>()?;
+            return Err(Error::other(nmcli_err));
+        }
+
+        Ok(cmd.stdout)
+    }
+}
+
+pub fn show_active_connections() -> Result<Vec<String>, Error> {
+    let mut nmcli = Nmcli::new();
+    nmcli
+        .with_global_args(vec!["-g", "NAME,DEVICE"])
+        .with_subcommands(vec!["connection", "show"])
+        .with_subcommand_args(vec!["--active"]);
+
+    let result = nmcli.exec()?;
+
+    let active_conn_device_pairs = result.lines().collect::<Result<Vec<String>, Error>>()?;
 
     Ok(active_conn_device_pairs)
 }
 
-pub fn get_wifi_status() -> Result<WiFiStatus, Error> {
-    let mut nmcli = Command::new("nmcli");
-    let cmd = nmcli.args(["-g", "WIFI"]).arg("g").output()?;
 
-    if !cmd.status.success() {
-        let nmcli_err = cmd.stderr.lines().collect::<Result<String, Error>>()?;
-        return Err(Error::other(nmcli_err));
     }
 
-    let status = cmd
-        .stdout
         .lines()
-        .take(1)
-        .collect::<Result<String, Error>>()?;
+pub fn get_wifi_status() -> Result<WiFiStatus, Error> {
+    let mut nmcli = Nmcli::new();
+    nmcli
+        .with_global_args(vec!["-g", "WIFI"])
+        .with_subcommands(vec!["g"]);
+
+    let result = nmcli.exec()?;
+
+    let status = result.lines().take(1).collect::<Result<String, Error>>()?;
 
     Ok(if status == "enabled" {
         WiFiStatus::Enabled
@@ -60,26 +101,21 @@ pub fn get_wifi_status() -> Result<WiFiStatus, Error> {
 }
 
 pub fn toggle_wifi(old_status: WiFiStatus) -> Result<WiFiStatus, Error> {
-    let mut nmcli = Command::new("nmcli");
-    let cmd = nmcli.arg("radio").arg("wifi");
+    let mut nmcli = Nmcli::new();
+    nmcli.with_subcommands(vec!["radio", "wifi"]);
 
     let new_status = match old_status {
         WiFiStatus::Enabled => {
-            cmd.arg("off");
+            nmcli.with_subcommand_args(vec!["off"]);
             WiFiStatus::Disabled
         }
         WiFiStatus::Disabled => {
-            cmd.arg("on");
+            nmcli.with_subcommand_args(vec!["on"]);
             WiFiStatus::Enabled
         }
     };
 
-    let cmd = cmd.output()?;
-
-    if !cmd.status.success() {
-        let nmcli_err = cmd.stderr.lines().collect::<Result<String, Error>>()?;
-        return Err(Error::other(nmcli_err));
-    }
+    let _ = nmcli.exec()?;
 
     Ok(new_status)
 }
