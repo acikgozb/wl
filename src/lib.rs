45cdd1cffc6853e60c8189d640b0d762f4b1c9ae
acikgozb <acikgozb@proton.me>
2025-05-25 23:48:03 +0300

    feat(lib): Reorganize public lib API exports
    Instead of only exporting subcommand modules, the adapter and network
    backend module are exported as well.
    
    This is done to:
    - Allow external adapter implementations,
    - Use `Nmcli` adapter directly for external flows.
    
    Signed-off-by: acikgozb <acikgozb@proton.me>

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,14 @@ mod scan;
 mod status;
 mod toggle;
 
-pub use adapter::Error as NetworkAdapterError;
-pub use connect::connect;
-pub use disconnect::disconnect;
+pub use adapter::{
+    CARRIAGE_RETURN, Decimal, Error as NetworkAdapterError, LINE_FEED, LOOPBACK_INTERFACE_NAME, Wl,
+};
+pub use connect::{Error as ConnectError, connect};
+pub use disconnect::{Error as DisconnectError, disconnect};
 pub use list_networks::list_networks;
-pub use scan::scan;
+pub use nmcli::Nmcli;
+pub use scan::{Error as ScanError, scan};
 pub use status::status;
 pub use toggle::toggle;
 
