1b3ed4255937b1429af08fc9f71cea78ffd965c5
acikgozb <acikgozb@proton.me>
2025-05-25 23:52:16 +0300

    feat(lib): Remove lib crate `Error`
    This Error only exists for `write_bytes`, and it does not add useful
    context to the underlying `io::Error`.
    Therefore, the wrapper Error is removed completely.
    
    Signed-off-by: acikgozb <acikgozb@proton.me>

--- src/lib.rs
+++ src/lib.rs
@@ -19,28 +19,9 @@ pub use scan::{Error as ScanError, scan};
 pub use status::status;
 pub use toggle::toggle;
 
-use std::{error, fmt, io};
+use std::io;
 
-#[derive(Debug)]
-pub enum Error {
-    CannotWriteBuffer(io::Error),
-    CannotFlushWriter(io::Error),
-}
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::CannotWriteBuffer(err) => {
-                write!(f, "unable to write to the output stream: {}", err)
-            }
-            Error::CannotFlushWriter(err) => {
-                write!(f, "unable to flush the output stream: {}", err)
-            }
-        }
-    }
-}
-impl error::Error for Error {}
-
-fn write_bytes(f: &mut impl io::Write, buf: &[u8]) -> Result<(), Error> {
-    f.write_all(buf).map_err(Error::CannotWriteBuffer)?;
-    f.flush().map_err(Error::CannotFlushWriter)
+fn write_bytes(f: &mut impl io::Write, buf: &[u8]) -> Result<(), io::Error> {
+    f.write_all(buf)?;
+    f.flush()
 }
